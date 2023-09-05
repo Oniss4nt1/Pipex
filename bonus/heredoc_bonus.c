4efac33eb7793cc333269a8cba38f37ecb8848ce
Bruno Rodrigues onissanti <brunrodr@c3r6p1.42sp.org.br>
2023-09-05 18:56:54 -0300

    V4 - heredoc starts

--- bonus/heredoc_bonus.c
+++ bonus/heredoc_bonus.c
@@ -1,34 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   heredoc_bonus.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brunrodr <brunrodr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/05 18:48:37 by brunrodr          #+#    #+#             */
-/*   Updated: 2023/09/05 18:48:38 by brunrodr         ###   ########.fr       */
+/*   Created: 2023/09/05 18:49:34 by brunrodr          #+#    #+#             */
+/*   Updated: 2023/09/05 18:49:40 by brunrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
-
-int	main(int argc, char **argv, char **envp)
-{
-	t_pipex	pipex;
-
-	init_pipex(&pipex);
-	if (!check_args(argc, argv, &pipex))
-	{
-		cleanup(&pipex);
-		return (1);
-	}
-	ft_parse_cmds(argv, &pipex, envp);
-	if (!pipex.cmd_paths)
-	{
-		cleanup(&pipex);
-		return (1);
-	}
-	exec_cmds(&pipex);
-	cleanup(&pipex);
-	return (0);
-}
+#include "pipex_bonus.h"
